fa9dab924f6bd98c87f64fdf281918d4b40b39c4
Omar Sabry <ohsabry@gmail.com>
2014-07-19 16:08:24 -0700

    change tag cloud to count on tag frequencies

--- lib/acts_as_taggable_on/tags_helper.rb
+++ lib/acts_as_taggable_on/tags_helper.rb
@@ -1,13 +1,14 @@
 module ActsAsTaggableOn
   module TagsHelper
     # See the wiki for an example using tag_cloud.
-    def tag_cloud(tags, classes)
-      return [] if tags.empty?
+    def tag_cloud(taggings, classes)
+      return [] if taggings.empty?
 
-      max_count = tags.sort_by(&:taggings_count).last.taggings_count.to_f
+      max_count = taggings.sort_by(&:frequency).last.frequency.to_f
 
-      tags.each do |tag|
-        index = ((tag.taggings_count / max_count) * (classes.size - 1))
+      taggings.each do |tagging|
+        tag = tagging.tag
+        index = ((tagging.frequency/ max_count) * (classes.size - 1))
         yield tag, classes[index.nan? ? 0 : index.round]
       end
     end
